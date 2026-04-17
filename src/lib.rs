57ddc99d003e037209443180f6ad3fe36e488a52
Rahul <rahulkum94l94@gmail.com>
2026-04-17 19:21:58 +0530

    feat:added input.rs to our main file

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,10 @@ pub mod cube;
 pub mod sphere;
 pub mod renderer;
 pub mod math;
+pub mod input;
 
 pub use vertex::Vertex;
 pub use cube::Cube;
 pub use sphere::Sphere;
 pub use renderer::Renderer;
+pub use input::InputState;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use sdl3::event::Event;
 use sdl3::keyboard::Keycode;
 use std::time::Instant;
-use render_from_scratch::{Cube, Sphere, Renderer, Vertex};
+use render_from_scratch::{Cube, Sphere, Renderer, Vertex,InputState};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let sdl_context = sdl3::init()?;
@@ -24,23 +24,22 @@ let mut vec_cube = vec![
     Cube::new(ox, oy, 0.0, size, (0.0, 0.0, 0.0)),
     Cube::new(ox,  oy + size, 0.0, size, (0.0, 100.0, 0.0))
 ];
-    let mut vec_sphere = vec![Sphere::new(Vertex::new(ox+10.0 , oy+228.0, 0.0);, size / 2.0, 6, 10)];
+    let mut vec_sphere = vec![Sphere::new(Vertex::new(ox+10.0 , oy+228.0, 0.0), size / 2.0, 6, 10)];
 
     let renderer = Renderer::new(4);
-let mut last_time = Instant::now();
-
-'running: loop {
-    let now = Instant::now();
-    let dt = now.duration_since(last_time).as_secs_f32();
-    last_time = now;
-    for event in event_pump.poll_iter() {
-        match event {
-            Event::Quit { .. }
-            | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'running,
-            
-            _ => {}
+    let mut last_time = Instant::now();
+    let mut input = InputState::new();
+    'running: loop {
+        let now = Instant::now();
+        let dt = now.duration_since(last_time).as_secs_f32();
+        last_time = now;
+        for event in event_pump.poll_iter() {
+            input.handle_event(event);
         }
-    }
+
+        if input.quit {
+            break 'running;
+        }
     let speed = 1.5;
     for cube in &mut vec_cube {
         cube.tick(speed * dt);
